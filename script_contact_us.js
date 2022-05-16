0bf850b7cd10cb2eecdad1e3b34f9cf5d513d8a5
imankitkalirawana <009ankitkalirawana@gmail.com>
2022-05-16 21:35:21 +0530

    imankitkalirawana

---
+++ script_contact_us.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: ["Ankit", "a YouTuber", "a Developer", "a Blogger", "a Programmer", "a Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    loop: true
+});
